4e2fb70d3fb8f73c474852db8cb06c0b92c8d52e
Sangmin Lee <105434827+ddongbu@users.noreply.github.com>
2023-09-11 09:09:58 +0900

    09-11

--- next-react-tutorial-1-typescript/.storybook/preview.js
+++
@@ -1,8 +0,0 @@
-import { addDecorator } from '@storybook/react';
-import routerDecorator from "./router-decorator";
-
-addDecorator(routerDecorator);
-
-export const parameters = {
-    actions: { argTypesRegex: "^on[A-Z].*" }
-};
